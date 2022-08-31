28d4b26799a2f5afe0bd0a7bc855a3a90e219004
Bradley Hartlove <bhartlovecodes@gmail.com>
2022-08-31 01:27:16 +0000

    Week Two: Updated with stopwords filtering

--- mapper.py
+++ mapper.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 import sys
 
-# get all lines from stdin
-for line in sys.stdin:
-    # remove leading and trailing whitespace
-    line = line.strip()
+def read_stopwords(f):
+    stopwords = []
+    for line in f:
+        line = line.strip().decode('utf-8')
+        stopwords.append(line)
+    return stopwords
 
-    # split the line into words; splits on any whitespace
-    words = line.split()
+def main():
+    # get all lines from stdin
+    for line in sys.stdin:
+        # remove leading and trailing whitespace
+        line = line.strip()
 
-    # output tuples (word, 1) in tab-delimited format
-    for word in words:
-        print '%s\t%s' % (word, "1")
+        # lowercase contents
+        line = line.lower()
+
+        # split the line into words; splits on any whitespace
+        words = line.split()
+
+        # stop_words
+        stop_words = read_stopwords("english")
+
+        # output tuples (word, 1) in tab-delimited format
+        for word in words:
+            if word not in stop_words:
+                print '%s\t%s' % (word, "1")
+
+if __name__ == "__main__":
+    main()
